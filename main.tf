c5b41fc797021821bcd9ea65f64f72b2d9c65ec2
souritaray <100030958+souritaray@users.noreply.github.com>
2024-05-28 12:10:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,28 +2,10 @@ terraform {
   required_version = ">= 0.13"  # Specify the required Terraform version
 }
 
-provider "aws" {
-  region = "us-west-2"  # Specify the AWS region
-}
+provider "null" {}
 
-resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Specify the AMI ID of the instance
-  instance_type = "t2.micro"  # Specify the instance type
-
-  tags = {
-    Name = "terraform-example"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "echo 'Hello, World!'"
-    ]
-  }
-
-  connection {
-    type        = "ssh"
-    user        = "ec2-user"  # Specify the user for SSH connection
-    private_key = file("~/.ssh/id_rsa")  # Specify the path to your private key
-    host        = self.public_ip  # Use the public IP of the instance for SSH connection
+resource "null_resource" "hello_world" {
+  provisioner "local-exec" {
+    command = "echo Hello, World"
   }
 }
