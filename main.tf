194afdaf8e3a70a649ab6663d44af58815b92a3a
souritaray <100030958+souritaray@users.noreply.github.com>
2024-06-05 15:25:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,9 @@
-provider "null" {}
+# Resource definition
+resource "null_resource" "hello" {
+  
+}
 
-  connection {
-    type        = "ssh"
-    user        = "ec2-user"  # Specify the user for SSH connection
-    private_key = file("~/.ssh/id_rsa")  # Specify the path to your private key
-    host        = self.public_ip  # Use the public IP of the instance for SSH connection
-resource "null_resource" "hello_world" {
-  provisioner "local-exec" {
-    command = "echo Hello, World"
-  }
+# Output "Hello world" during the apply stage
+output "hello_world" {
+  value = "Hello world"
 }
