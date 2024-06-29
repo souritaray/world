caa0d12bae9d2d7bf9c7efd7501a43df8bc7675c
souritaray <100030958+souritaray@users.noreply.github.com>
2024-06-29 15:29:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,20 @@
-# Resource definition
-resource "null_resource" "hello" {
-  
+# Dummy resource 1
+resource "null_resource" "example1" {
+  provisioner "local-exec" {
+    command = "echo Example Resource 1"
+  }
 }
 
-# Output "Hello world" during the apply stage
-output "hello_world" {
-  value = "Hello world good"
+# Dummy resource 2
+resource "null_resource" "example2" {
+  provisioner "local-exec" {
+    command = "echo Example Resource 2"
+  }
+}
+
+# Dummy resource 3
+resource "null_resource" "example3" {
+  provisioner "local-exec" {
+    command = "echo Example Resource 3"
+  }
 }
