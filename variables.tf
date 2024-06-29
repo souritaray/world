fc9fc5f1e556d4c262a0227b40f0e173346ed4ea
souritaray <100030958+souritaray@users.noreply.github.com>
2024-06-29 15:58:35 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,17 +1,17 @@
 variable "example_string" {
   description = "An example string variable"
   type        = string
-  default     = "Hello, Terraform!"
+  default     = "Hello, default!"
 }
 
 variable "example_number" {
   description = "An example number variable"
   type        = number
-  default     = 42
+  default     = 00
 }
 
 variable "example_list" {
   description = "An example list variable"
   type        = list(string)
-  default     = ["one", "two", "three"]
+  default     = ["deafult", "tvar", "list"]
 }
