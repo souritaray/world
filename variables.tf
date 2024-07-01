a5e5a5f6a108a9e7f707408eff7630e7551e6698
souritaray <100030958+souritaray@users.noreply.github.com>
2024-07-01 14:50:19 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,17 +1,14 @@
 variable "example_string" {
   description = "An example string variable"
   type        = string
-  default     = "Hello, default!"
 }
 
 variable "example_number" {
   description = "An example number variable"
   type        = number
-  default     = 00
 }
 
 variable "example_list" {
   description = "An example list variable"
   type        = list(string)
-  default     = ["deafult", "tvar", "list"]
 }
